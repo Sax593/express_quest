1e30512824de7f41cec97a420f366a4cdc7acfba
WAQUET Valentin <waquetv@gmail.com>
2022-11-07 10:25:27 +0100

    Create post query for user and movie

--- movieHandlers.js
+++ movieHandlers.js
@@ -56,39 +56,24 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.status(200).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
-};
-
-const getUserById = (req, res) => {
-  const id = parseInt(req.params.id);
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
 
   database
-    .query("select * from users where id = ?", [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.status(404).send("Not Found");
-      }
+    .query("INSERT INTO movies(title, director, year, color, duration) VALUES (?,?,?,?,?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
     })
     .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      console.log(err);
+      res.status(500).send("Error saving the movie");
     });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUserById
+  postMovie
 };
